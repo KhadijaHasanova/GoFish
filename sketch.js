11d2d4b92a06882d3514767d2badb4cfc2dc34fb
KhadijaHasanova <khadijahasanova28@gmail.com>
2021-04-02 18:00:32 -0500

    Game State

--- sketch.js
+++ sketch.js
@@ -12,9 +12,15 @@ var gameState = 0;
  
 var score = 0;
  
-var greyBar,scoreBar,scoreCarrot;
+var greyBar, scoreBar, scoreCarrot;
 
 var easy, medium, hard;
+
+var ateSound, hitSharkSound, gameOverSound, wonSound;
+
+var reset;
+
+var edges;
  
 function preload() {
  //load the animation for the fish
@@ -32,15 +38,27 @@ function preload() {
  
  //load the image for game over
  gameOverImg = loadImage("images/gameOver.png");
+
+ //load the sound for eating food
+ ateSound = loadSound("sounds/ateFoodSound.wav");
+
+ //load the sound for hitting the shark
+ hitSharkSound = loadSound("sounds/hitShark.wav");
+
+ //load the sound for gameOver
+ gameOverSound = loadSound("sounds/gameOverSound.wav");
+
+ //load the sonud for winning
+ wonSound = loadSound("sounds/wonSound.wav");
 }
  
 function setup() {
  //create the canvas
- createCanvas(1200,600);
+ createCanvas(windowWidth,windowHeight);
  
  //set the background image
- ground = createSprite(600,300,200,200);
- ground.scale = 2.1;
+ ground = createSprite(width,height-500,200,200);
+ ground.scale = 3.5;
  ground.addImage(backgroundImg);
  
  //create the fish and add the animations
@@ -49,7 +67,7 @@ function setup() {
  fish.addAnimation("fishLeft",fishLeft);
  
  //create the gameOver sprite
- gameOver = createSprite(600,300);
+ gameOver = createSprite(width/2-0,height/2-150);
  gameOver.addImage(gameOverImg);
  gameOver.scale = 2;
  
@@ -68,6 +86,56 @@ function setup() {
  scoreCarrot.addImage(carrotImg);
  scoreCarrot.depth = fish.depth - 1;
 
+ //add color to the buttons
+ let color1 = color(17, 255, 0);
+ let color2 = color(209, 209, 0);
+ let color3 = color(255, 13, 0);
+ let color4 = color(255, 73, 56);
+
+ //create the button for the easy level
+ easy = createButton("EASY");
+ easy.position(width/2-300,height/2-50);
+ easy.style('background-color', color1);
+ easy.style('font-size', '40px');
+ easy.style('font-family', 'Architects Daughter');
+ easy.style('border-radius','40px');
+ easy.style('border-width','10px');
+ easy.style('border-color','white');
+ easy.style('border-style','outset double outset double');
+
+ //create the button for the medium level
+ medium = createButton("MEDIUM");
+ medium.position(width/2-70,height/2-50);   
+ medium.style('background-color', color2);
+ medium.style('font-size', '40px');
+ medium.style('font-family', 'Architects Daughter');
+ medium.style('border-radius','40px');
+ medium.style('border-width','10px');
+ medium.style('border-color','white');
+ medium.style('border-style','outset double outset double');
+
+ //create the button for the hard level
+ hard = createButton("HARD");
+ hard.position(width/2+200,height/2-50);
+ hard.style('background-color', color3);
+ hard.style('font-size', '40px');
+ hard.style('font-family', 'Architects Daughter');
+ hard.style('border-radius','40px');
+ hard.style('border-width','10px');
+ hard.style('border-color','white');
+ hard.style('border-style','outset double outset double');
+
+ //create the reset button
+ reset = createButton("RESET");
+ reset.position(width/2-70,height/2);
+ reset.style('background-color', color4);
+ reset.style('font-size', '40px');
+ reset.style('font-family', 'Architects Daughter');
+ reset.style('border-radius','40px');
+ reset.style('border-width','10px');
+ reset.style('border-color','white');
+ reset.style('border-style','outset double outset double');
+
  //create the obstacle  group
  obstacleGroup = new Group();
  
@@ -79,51 +147,49 @@ function draw() {
   //set the background color
   background(0,0,0);
 
+  //create edge sprites
+  edges = createEdgeSprites();
+  fish.collide(edges);
+
   //starting screen
   if(gameState === 0) {
   //set the background color
   background(0,0,0);
+
+  //show the level buttons
+  easy.show();
+  medium.show();
+  hard.show();
  
   //display the game name
   strokeWeight(5);
   stroke("blue");
-  textSize(80);
+  textSize(100);
+  textFont('Architects Daughter');
   fill("pink");
-  text("Go Fish",420,100);
+  text("Go Fish",width/2-150,height/2-370);
  
   //display the game information
   noStroke();
   textSize(30);
-  text("Use the arrow keys to move the fish",310,200);
-  text("Dodge the sharks and eat the carrots",305,250);
-  text("Don’t run out of energy",405,300);
-  text("Click a level to start",430,350);
-
-  //create the buttons for different levels
-  easy = createButton("EASY");
-  easy.position(200,500);
-
-  medium = createButton("MEDIUM");
-  medium.position(500,500);   
-   
-  hard = createButton("HARD");
-  hard.position(800,500);
+  textFont('Architects Daughter');
+  text("Use the arrow keys to move the fish",width/2-240,height/2-300);
+  text("Dodge the sharks and eat the carrots",width/2-240,height/2-250);
+  text("Don’t run out of energy",width/2-140,height/2-200);
+  text("Click a level to start",width/2-120,height/2-150);
 
   //go to the easy level
   easy.mousePressed(()=>{
-    console.log("easy");
     gameState = 1;
   });
 
   //go to the medium level
   medium.mousePressed(()=>{
-    console.log("medium");
     gameState = 2;
   });
 
   //go to the hard level
   hard.mousePressed(()=>{
-    console.log("hard");
     gameState = 3;
   })
 
@@ -134,11 +200,20 @@ function draw() {
   scoreBar.visible = false;
   greyBar.visible = false;
   scoreCarrot.visible = false;
+
+  //hide the reset button
+  reset.hide();
  }
  
 
  //easy level
  if(gameState === 1) {
+   //hide the buttons
+  easy.hide();
+  medium.hide();
+  hard.hide();
+  reset.hide();
+
   //reset the background
   ground.velocityX = -3;
   if(ground.x < 200) {
@@ -186,37 +261,57 @@ function draw() {
     foodGroup.destroyEach();
 
     //increase x position half of incresement of width
-    scoreBar.x = scoreBar.x + 5;
-    scoreBar.width = scoreBar.width + 10;
+    scoreBar.x = scoreBar.x + 10;
+    scoreBar.width = scoreBar.width + 20;
+
+    //play the sound for eating food
+    ateSound.play();
   }
 
   //decrease the score of the fish as time goes on
   if(frameCount % 200 === 0) {
-    scoreBar.x = scoreBar.x - 5;
-    scoreBar.width = scoreBar.width - 10;
+    scoreBar.x = scoreBar.x - 2;
+    scoreBar.width = scoreBar.width - 4;
   }
 
   //destroy the shark and decrease the energy level
   if(obstacleGroup.isTouching(fish)) {
     obstacleGroup.destroyEach();
-    scoreBar.x = scoreBar.x - 10;
-    scoreBar.width = scoreBar.width - 20;
+    scoreBar.x = scoreBar.x - 3;
+    scoreBar.width = scoreBar.width - 6;
+
+    //play the sound for hitting the shark
+    hitSharkSound.play();
   }
 
   //let the player win when the energy bar is full
   if(scoreBar.width >= 300) {
+    //change the game state
     gameState = 5;
+
+    //play the won sound
+    wonSound.play();
   }
 
   //end the game when there is no energy left
-  if(scoreBar.width <= 0) {
+  if(scoreBar.width <= 150) {
+    //change the game state
     gameState = 4;
+
+    //play the game over sound
+    gameOverSound.play();
   }
 }
 
 
 //medium level
 if(gameState === 2) { 
+  //hide the buttons
+  easy.hide();
+  medium.hide();
+  hard.hide();
+  reset.hide();
+
   //reset the background
   ground.velocityX = -3;
   if(ground.x < 200) {
@@ -266,35 +361,55 @@ if(gameState === 2) {
     //increase x position half of incresement of width
     scoreBar.x = scoreBar.x + 5;
     scoreBar.width = scoreBar.width + 10;
+
+    //play the sound for eating food
+    ateSound.play();
   }
 
   //decrease the score of the fish as time goes on
   if(frameCount % 200 === 0) {
-    scoreBar.x = scoreBar.x - 5;
-    scoreBar.width = scoreBar.width - 10;
+    scoreBar.x = scoreBar.x - 2;
+    scoreBar.width = scoreBar.width - 4;
   }
 
   //destroy the shark and decrease the energy level
   if(obstacleGroup.isTouching(fish)) {
     obstacleGroup.destroyEach();
-    scoreBar.x = scoreBar.x - 10;
-    scoreBar.width = scoreBar.width - 20;
+    scoreBar.x = scoreBar.x - 6;
+    scoreBar.width = scoreBar.width - 12;
+
+    //play the sound for hitting the shark
+    hitSharkSound.play();
   }
 
   //let the player win when the energy bar is full
   if(scoreBar.width >= 300) {
+    //change the game state
     gameState = 5;
+
+    //play the won sound
+    wonSound.play();
   }
 
   //end the game when there is no energy left
   if(scoreBar.width <= 0) {
+    //change the game state
     gameState = 4;
+
+    //play the game over sound
+    gameOverSound.play();
   }
 }
  
 
 //hard level
 if(gameState === 3) { 
+  //hide the buttons
+  easy.hide();
+  medium.hide();
+  hard.hide();
+  reset.hide();
+  
   //reset the background
   ground.velocityX = -3;
   if(ground.x < 200) {
@@ -342,40 +457,57 @@ if(gameState === 3) {
     foodGroup.destroyEach();
 
     //increase x position half of incresement of width
-    scoreBar.x = scoreBar.x + 5;
-    scoreBar.width = scoreBar.width + 10;
+    scoreBar.x = scoreBar.x + 9;
+    scoreBar.width = scoreBar.width + 18;
+    
+    //play the sound for eating food
+    ateSound.play();
   }
 
   //decrease the score of the fish as time goes on
   if(frameCount % 200 === 0) {
-    scoreBar.x = scoreBar.x - 5;
-    scoreBar.width = scoreBar.width - 10;
+    scoreBar.x = scoreBar.x - 2;
+    scoreBar.width = scoreBar.width - 4;
   }
 
   //destroy the shark and decrease the energy level
   if(obstacleGroup.isTouching(fish)) {
     obstacleGroup.destroyEach();
-    scoreBar.x = scoreBar.x - 10;
-    scoreBar.width = scoreBar.width - 20;
+    scoreBar.x = scoreBar.x - 8;
+    scoreBar.width = scoreBar.width - 16;
+
+    //play the sound for hitting the shark
+    hitSharkSound.play();
   }
 
   //let the player win when the energy bar is full
-  if(scoreBar.width >= 300) {
+  if(scoreBar.width >= 150) {
+    //change the game state
     gameState = 5;
+
+    //play the won sound
+    wonSound.play();
   }
 
   //end the game when there is no energy left
   if(scoreBar.width <= 0) {
+    //change the game state
     gameState = 4;
+
+    //play the game over sound
+    gameOverSound.play();
   }
 }
 
 
  //player lost
  if(gameState === 4) {
-   //display the gameOverImg
+   //display the gameOverImg and resetImg
    gameOver.visible = true;
- 
+   
+   //display the reset button
+   reset.show();
+
    //make everything invisible
    fish.visible = false;
    ground.visible = false;
@@ -385,9 +517,9 @@ if(gameState === 3) {
    obstacleGroup.destroyEach();
    foodGroup.destroyEach();
 
-   //create the reset button
-   //reset = createButton("RESET");
-   //reset.position(400,400);
+   reset.mousePressed(()=>{
+    gameState = 0;
+  })
  }
  
  //player won
@@ -400,32 +532,36 @@ if(gameState === 3) {
    scoreCarrot.visible = false;
    obstacleGroup.destroyEach();
    foodGroup.destroyEach();
- 
+
+   //display the reset button
+   reset.show();
+
    //display that the player won
-   textSize(80);
    fill("pink");
    strokeWeight(4);
    stroke("blue");
-   text("YOU WON!!!",400,300);
+   textSize(180);
+   textFont('Architects Daughter');
+   text("YOU WON !!!",width/2-450,height/2-100);
+
+   reset.mousePressed(()=>{
+    gameState = 0;
+  })
  }
  
  //draw the sprites
  drawSprites();
 }
+
  
 function easySharks() {
   if(frameCount % 300 === 0) {
     var randY = Math.round(random(80,520));
-    shark = createSprite(1150,randY,200,200);
+    shark = createSprite(1200,randY,200,200);
     shark.addImage(sharkImg);
     shark.scale = 0.8;
-    shark.lifetime = 350;
-  
-    if(scoreBar.width <= 170) {
-      shark.velocityX = -5;
-    } else {
-      shark.velocityX = -10;
-    }
+    shark.lifetime = 400;
+    shark.velocityX = -5;
     obstacleGroup.add(shark);
   }
  }
@@ -445,16 +581,11 @@ function easySharks() {
  function mediumSharks() {
   if(frameCount % 250 === 0) {
     var randY = Math.round(random(80,520));
-    shark = createSprite(1150,randY,200,200);
+    shark = createSprite(1200,randY,200,200);
     shark.addImage(sharkImg);
     shark.scale = 0.8;
-    shark.lifetime = 350;
-  
-    if(scoreBar.width <= 170) {
-      shark.velocityX = -6;
-    } else {
-      shark.velocityX = -12;
-    }
+    shark.lifetime = 400;
+    shark.velocityX = -6;
     obstacleGroup.add(shark);
   }
  }
@@ -474,16 +605,11 @@ function easySharks() {
 function hardSharks() {
   if(frameCount % 200 === 0) {
     var randY = Math.round(random(80,520));
-    shark = createSprite(1150,randY,200,200);
+    shark = createSprite(1200,randY,200,200);
     shark.addImage(sharkImg);
     shark.scale = 0.8;
-    shark.lifetime = 350;
-  
-    if(scoreBar.width <= 170) {
-      shark.velocityX = -7;
-    } else {
-      shark.velocityX = -14;
-    }
+    shark.lifetime = 400;
+    shark.velocityX = -7;
     obstacleGroup.add(shark);
   }
  }
